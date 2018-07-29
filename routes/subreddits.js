0d9eb6202d8036a096730793b0b6d3d46bb50088
Cellv2 <rich.a@live.com>
2018-07-29 19:27:23 +0100

    Initial pass over routes

---
+++ routes/subreddits.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+//root landing route
+router.get("/", function(req, res) {
+    res.render("landing");
+});
+
+//register route
+
+//login route
+
+//login redirect logic
+
+//logout route
+
+module.exports = router;
